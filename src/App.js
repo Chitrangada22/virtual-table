f95afbf087a62257747692dc5ed20c34466d5125
Chitrangada Pradhan <chitrangadapradhan@Chitrangadas-MacBook-Pro.local>
2023-12-21 19:22:19 +0000

    adding JS and CSS
    Creation of virtualized table

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
 import './App.css';
+import 'react-virtualized/styles.css';
+
+import { Column, Table } from 'react-virtualized';
+import React, { useEffect, useState } from 'react';
+
+import axios from 'axios';
 
 function App() {
+  const [showTable, setShowTable] = useState(false);
+  const [data, setData] = useState([]);
+  const url = 'http://universities.hipolabs.com/search?country=United+States';
+
+  const fetchInfo = () => {
+    return axios.get(url).then((res) => setData(res.data));
+  };
+
+  useEffect(() => {
+    fetchInfo();
+  }, []);
+
+  const handleClick = async () => {
+    console.log(data);
+    !!data && setShowTable(true);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <button
+        className="App-link"
+        onClick={handleClick}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Load Universities
+      </button>
+      {showTable && !!data &&
+      <div className='VirtualizedTable'>
+        <Table
+          className='table-css'
+          width={500}
+          height={1000}
+          headerHeight={50}
+          rowHeight={100}
+          rowCount={data.length}
+          rowGetter={({ index }) => data[index]}
         >
-          Learn React
-        </a>
-      </header>
+          <Column
+            width={200}
+            label='Name of University'
+            dataKey='name'
+          />
+          <Column
+            label='Country'
+            dataKey='country'
+            width={200}
+          />
+
+        </Table>
+        </div>}
+
     </div>
   );
 }
